b151b4074af45745541cff6e461e3508b3d1d115
Grant Winney <1354059+grantwinney@users.noreply.github.com>
2019-10-04 22:36:36 -0400

    a simple app to test the github package repository

---
+++ HelloWorld/AvengerQuotes.cs
@@ -0,0 +1,21 @@
+﻿namespace HelloWorld
+{
+    public class AvengerQuotes
+    {
+        public static string CaptainAmerica => "I can do this all day.";
+
+        public static string Vision => "A thing isn't beautiful because it lasts.";
+
+        public static string Loki => "I assure you, brother, the sun will shine on us again!";
+
+        public static string Thor => "I'm not sad, you idiot. I'm pissed off! Angry. I lost my father. I lost my hammer.";
+
+        public static string Banner => "That's my secret, Cap... I'm always angry.";
+
+        public static string Coulson => "Don't ever tell me there's no way.";
+
+        public static string Fury => "Until such time as the world ends, we will act as though it intends to spin on.";
+
+        public static string Korg => "I tried to start a revolution, but didn't print enough pamphlets.";
+    }
+}
